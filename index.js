24b065b58223d5da66bc6aa6a4a7a959df4dd7ae
Milco Numan <milco.numan@gmail.com>
2018-01-30 19:48:08 +0100

    Refactoring the app to later ...

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express()
+const articles = [{ title:'Example'}];
 
-const port = process.env.PORT || 3000;
+app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req,res) => {
-    res.send('Hello World');
+app.get('/articles', (req,res) => {
+    res.send(articles);
 });
 
-app.listen(port, () => console.log(`Express web app is running at localhost:${port}`));
+app.post('/articles', (req,res,next) => {
+    res.send('OK');
+});
+
+app.get('/articles/:id', (req,res,next) => {
+    const id = req.params.id;
+    console.log('Fetching ', id);
+    res.send(articles[id]);
+});
+
+app.delete('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('Deleting:', id);
+    delete articles[id];
+    res.send({ Message: 'Deleted'});
+});
+
+app.listen(app.get('port'), () => console.log('Express app started on port ', app.get('port')));
