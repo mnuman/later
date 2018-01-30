8dace84772008f31cce764519e500e06dc6eecfb
Milco Numan <milco.numan@gmail.com>
2018-01-30 20:16:53 +0100

    Switching to sqlite3 for storage

--- index.js
+++ index.js
@@ -2,32 +2,36 @@ const express = require('express');
 const app = express()
 const articles = [{ title:'Example'}];
 const bodyParser = require('body-parser');
+const Article = require('./db').Article;
 
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/articles', (req,res) => {
-    res.send(articles);
-});
-
-app.post('/articles', (req,res,next) => {
-    const article = {title: req.body.title};
-    articles.push(article);
-    res.send(article);
+app.get('/articles', (req,res,next) => {
+    Article.all((err, articles) => {
+        if (err) return next(err);
+        res.send(articles);
+    });
 });
 
 app.get('/articles/:id', (req,res,next) => {
     const id = req.params.id;
-    console.log('Fetching ', id);
-    res.send(articles[id]);
+    Article.find(id, (err,article) => {
+        if (err) return next(err);
+        res.send(article);
+    });
 });
 
 app.delete('/articles/:id', (req, res, next) => {
     const id = req.params.id;
-    console.log('Deleting:', id);
-    delete articles[id];
-    res.send({ Message: 'Deleted'});
+    Article.delete(id, (err) => {
+        if (err) return next(err);
+        res.send({ Message: 'Deleted'});
+    })
 });
 
 app.listen(app.get('port'), () => console.log('Express app started on port ', app.get('port')));
+
+module.exports = app;
+
