f84c8ccc57ed91c7625b88cd91aa34c4c4ee8176
Milco Numan <milco.numan@gmail.com>
2018-01-30 20:27:06 +0100

    Completed backend

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const express = require('express');
 const app = express()
-const articles = [{ title:'Example'}];
+const articles = [{ title: 'Example' }];
 const bodyParser = require('body-parser');
 const Article = require('./db').Article;
+const read = require('node-readability');
 
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/articles', (req,res,next) => {
+app.get('/articles', (req, res, next) => {
     Article.all((err, articles) => {
         if (err) return next(err);
         res.send(articles);
     });
 });
 
-app.get('/articles/:id', (req,res,next) => {
+app.get('/articles/:id', (req, res, next) => {
     const id = req.params.id;
-    Article.find(id, (err,article) => {
+    Article.find(id, (err, article) => {
         if (err) return next(err);
         res.send(article);
+    });
+});
+
+app.post('/articles', (req, res, next) => {
+    const url = req.body.url;
+    read(url, (err, result) => {
+        if (err || !result) res.status(500).send('Error downloading article');
+        Article.create({
+            title: result.title, content: result.content
+        }, (err, article) => {
+            if (err) return next(err);
+            res.send('OK');
+        });
     });
 });
 
@@ -27,7 +41,7 @@ app.delete('/articles/:id', (req, res, next) => {
     const id = req.params.id;
     Article.delete(id, (err) => {
         if (err) return next(err);
-        res.send({ Message: 'Deleted'});
+        res.send({ Message: 'Deleted' });
     })
 });
 
