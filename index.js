2aaf339169cc0ca9c73bc50e34b91163592a5e99
Milco Numan <milco.numan@gmail.com>
2018-01-30 19:54:38 +0100

    Implementing POST operation

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express()
 const articles = [{ title:'Example'}];
+const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/articles', (req,res) => {
     res.send(articles);
 });
 
 app.post('/articles', (req,res,next) => {
-    res.send('OK');
+    const article = {title: req.body.title};
+    articles.push(article);
+    res.send(article);
 });
 
 app.get('/articles/:id', (req,res,next) => {
